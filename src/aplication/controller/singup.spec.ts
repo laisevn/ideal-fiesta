45d8155a48c8d26fb0e564a122bad5eba160be50
Laíse <laisevn@gmail.com>
2021-05-02 16:58:38 -0300

    test: ensure SignUpController calls IEmailValidator with corretly email

--- src/aplication/controller/singup.spec.ts
+++ src/aplication/controller/singup.spec.ts
@@ -69,4 +69,20 @@ describe('SingUpController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamsError('email'))
   })
+
+  test('Should call IEmailValidator with correct email', () => {
+    const { controller, emailValidatorStub } = makeController()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpResquest = {
+      body: {
+        displayName: 'Fulano de Tal',
+        email: 'oneemail@email.com',
+        password: 'one_password'
+      }
+    }
+
+    controller.handle(httpResquest)
+    expect(isValidSpy).toHaveBeenCalledWith('oneemail@email.com')
+  })
 })
