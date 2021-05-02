7322f0bc196564c1988c14d26604aeb2583c61f7
Laíse <laisevn@gmail.com>
2021-05-02 16:52:06 -0300

    test: create mock to email validator

--- src/aplication/controller/singup.spec.ts
+++ src/aplication/controller/singup.spec.ts
@@ -1,9 +1,32 @@
 import { MissingParamsError } from '../presentation/errors/missingParamsError'
 import { SingUpController } from './singup'
+import { InvalidParamsError } from '../presentation/errors/invalidParamError'
+import { IEmailValidator } from '../presentation/IEmailValidator'
+
+
+interface ControllerTypes {
+  controller: SingUpController
+  emailValidatorStub: IEmailValidator
+}
+
+const makeController = (): ControllerTypes => {
+  class EmailValidatorStub implements IEmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  const controller = new SingUpController(emailValidatorStub)
+
+  return {
+    controller,
+    emailValidatorStub
+  }
+}
 
 describe('SingUpController', () => {
   test('Should return 400 if no email is provided', () => {
-    const controller = new SingUpController()
+    const { controller } = makeController()
     const httpResquest = {
       body: {
         displayName: 'Fulano de Tal',
@@ -17,7 +40,7 @@ describe('SingUpController', () => {
   })
 
   test('Should return 400 if no password is provided', () => {
-    const controller = new SingUpController()
+    const { controller } = makeController()
     const httpResquest = {
       body: {
         displayName: 'Fulano de Tal',
@@ -28,5 +51,22 @@ describe('SingUpController', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamsError('password'))
+  })
+
+  test('Should return 400 if an invalid email is provided', () => {
+    const { controller, emailValidatorStub } = makeController()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+    const httpResquest = {
+      body: {
+        displayName: 'Fulano de Tal',
+        email: 'invalid_oneemail@email.com',
+        password: 'one_password'
+      }
+    }
+    const httpResponse = controller.handle(httpResquest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamsError('email'))
   })
 })
