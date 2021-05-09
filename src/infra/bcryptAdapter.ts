2dd8934deb9386875dbe3d913d64456caa606186
Laíse <laisevn@gmail.com>
2021-05-09 14:23:27 -0300

    feat: add infra layer and adpter to bcrypt lib

---
+++ src/infra/bcryptAdapter.ts
@@ -0,0 +1,14 @@
+import { IEncrypter } from '../data/protocols/IEncrypter'
+import bcrypt from 'bcrypt'
+
+export class BcryptAdapter implements IEncrypter {
+  private readonly salt: number
+
+  constructor (salt: number) {
+    this.salt = salt
+  }
+  async encrypt (value: string): Promise<string> {
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
+  }
+}
