f319c7c069ada625ae3c4aeb8fc4e1bba905156f
Laíse <laisevn@gmail.com>
2021-05-09 17:29:04 -0300

    feat: ensure requests return json content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { bodyParser } from '../middlewares/bodyParser'
 import { Express } from 'express'
 import { cors } from '../middlewares/cors'
+import { contentType } from '../middlewares/contentType'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
