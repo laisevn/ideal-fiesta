40e8fc2c820627438531e8d6214dcd5e7e3d3ead
Laíse <laisevn@gmail.com>
2021-05-09 17:34:21 -0300

    refactor: group middlewares import

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
-import { bodyParser } from '../middlewares/bodyParser'
+import { bodyParser, cors, contentType } from '../middlewares'
 import { Express } from 'express'
-import { cors } from '../middlewares/cors'
-import { contentType } from '../middlewares/contentType'
 
 export default (app: Express): void => {
   app.use(bodyParser)
