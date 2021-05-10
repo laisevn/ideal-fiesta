f513e479a9524d129fdb41f644bd0bb19813884b
Laíse <laisevn@gmail.com>
2021-05-10 03:41:29 -0300

    feat: ensure Signup route returns ad

---
+++ src/main/config/routes.ts
@@ -0,0 +1,10 @@
+import { Express, Router } from 'express'
+import fg from 'fast-glob'
+
+export default (app: Express): void => {
+  const router = Router()
+  app.use(router)
+  fg.sync('**/src/main/routes/**Routes.ts').map(async file => {
+    (await import(`../../../${file}`)).default(router)
+  })
+}
